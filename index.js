fee48da01272f975a7b3c708f000c1f3af98bdad
Mohamad Chebli <mchebli008@hotmail.com>
2024-06-27 14:48:22 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const XLSX = require("xlsx");
+const OpenAI = require("openai");
+require("dotenv").config();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+async function rateConversations(filePath) {
+  const workbook = XLSX.readFile(filePath);
+  const sheetName = workbook.SheetNames[0];
+  const worksheet = workbook.Sheets[sheetName];
+  const conversations = XLSX.utils.sheet_to_json(worksheet);
+
+  const conversationRatings = [];
+
+  for (const conversation of conversations) {
+    const conversationText = conversation["Conversation"];
+
+    const conversationData = await rateConversation(conversationText);
+    conversationRatings.push(conversationData);
+  }
+  XLSX.writeFile(filePath, conversationRatings);
+  return conversationRatings;
+}
+
+async function rateConversation(conversationText) {
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: `You will be provided with a converation between an AI agent and a user, your task is to classify each conversation with either Excellent, Good, Average, Poor, Terrible: ${conversationText}`,
+      },
+    ],
+    temperature: 0.7,
+    max_tokens: 64,
+    top_p: 1,
+  });
+
+  const rating = response.choices[0].message;
+  const usage = response.usage;
+
+  const conversationData = {
+    rating: rating,
+    usage: usage,
+  };
+
+  console.log("data: ", conversationData);
+  return conversationData;
+}
+
+(async () => {
+  const filePath = "./SampleConvos.xlsx";
+  const ratings = await rateConversations(filePath);
+})();
